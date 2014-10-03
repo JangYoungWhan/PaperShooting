a74c8d3316b4c67e185acba5d905452f6a2d85c4
JangYoungHwan <stdjangyounghwan@gmail.com>
2014-10-03 20:05:14 +0900

    Add a project named Math

---
+++ Math/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+	/// <summary>
+	/// Total hitpoints
+	/// </summary>
+	public int hp = 1;
+
+	/// <summary>
+	/// Enemy or Player?
+	/// </summary>
+	public bool isEnemy = true;
+
+	public void Damage(int damageCount)	{
+		hp -= damageCount;
+
+		if (hp <= 0) {
+			// Effect
+			SpecialEffectsHelper.Instance.Explosion(transform.position);
+
+			// Sound
+			SoundEffectsHelper.Instance.MakeExplosionSound();
+
+			// Dead!
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider)	{
+		// Is this a shot?
+		ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript> ();
+		if (shot != null) {
+			// Avoid friendly fire
+			if (shot.isEnermyShot != isEnemy) {
+				Damage (shot.damage);
+
+				// Destroy the shot
+				// Remember to always target the game object,
+				// otherwise you will just remove the script
+				Destroy (shot.gameObject);
+			}
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Math/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponScript : MonoBehaviour {
+	
+	// Projectile prefab for shooting
+	public Transform shotPrefab;
+	
+	// Cooldown in seconds between two shots
+	public float shootingRate = 0.25f;
+
+	// Cooldown
+	private float shootCooldown;
+
+	// Use this for initialization
+	void Start () {
+		shootCooldown = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shootCooldown > 0) {
+			shootCooldown -= Time.deltaTime;
+		}
+	}
+
+	// Shooting from another script.
+	// Create a new projectile if possible
+	public void Attack(bool isEnemy)
+	{
+		if(CanAttack)
+		{
+			shootCooldown = shootingRate;
+
+			// Create a new shot
+			var shotTransform = Instantiate(shotPrefab) as Transform;
+
+			// Assign position
+			shotTransform.position = transform.position;
+
+			// The is enemy property
+			ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+			if(shot != null)
+			{
+				shot.isEnermyShot = isEnemy;
+			}
+
+			// Make the weapon shot always towards it
+			MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+			if(move != null)
+			{
+				// towards in 2D space is the right of the sprite
+				move.direction = this.transform.right;
+			}
+		}
+	}
+
+	// Is the weapon ready to create a new projectile?
+	public bool CanAttack
+	{
+		get
+		{
+			return shootCooldown <= 0.0f;
+		}
+	}
+}
